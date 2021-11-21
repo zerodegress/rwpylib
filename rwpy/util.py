130238b1e236a27bef93484943c1e0fc991bc751
IceDegress <zerodegress@outlook.com>
2021-11-21 09:28:21 +0800

    代码表加载相关

--- rwpy/util.py
+++ rwpy/util.py
@@ -2,9 +2,10 @@ import os
 import json
 
 from rwpy.errors import VisitAbstractMemberError
+from typing import List,Tuple
 
 
-def filterl(func,lst):
+def filterl(func,lst) -> list:
     '''filter，但返回list'''
     return list(filter(func,lst))
 
@@ -12,11 +13,38 @@ def filterl(func,lst):
 def check(obj,type,message: str='参数类型错误'):
     '''快速检查参数类型错误'''
     if not isinstance(obj,type):
-        raise TypeError()
+        raise TypeError(message)
+
+
+def CodeList(object):
+    '''代码表'''
+    def __init__(self,codelist_src: dict):
+        check(codelist_src,dict)
+        self.__src = codelist_src
+        self.__namecheck = map(lambda x: (x['key'],x['section']),codelist_src['attributes'])
+    
+    
+    @property
+    def namecheck() -> List[Tuple[str,str]]:
+        '''
+        包含若干校验元素的校验名表，每个校验元素为2长度的元组，
+        0位置为代码名，1位置为段落名
+        '''
+        return self.__namecheck[:]
+    
+    
+    def getcodes(self,sec_name: str=None):
+        '''返回段落下所有代码。不填参数默认为返回所有代码'''
+        if sec_name is None:
+            return self.__src['attributes'][:]
+        check(sec_name,str)
+        return filterl(lambda x: x['section'] == sec_name,self.__src['attributes'])
+    
+
 
 
 class Builder(object):
-
+    '''抽象类'''
     def __init__(self,template=None):
         pass
             
@@ -26,32 +54,12 @@ class Builder(object):
         raise VisitAbstractMemberError()
         
 
-class CodeList(object):
-    
-    def __init__(self):
-        self.elements = []
-        self.sections = []
-        self.types = []
-    __slots__ = ('author','description','version','game_version')
-
-
-class JSONObject(dict):
-
-    def __getattr__(self,attr):
-        return self[attr]
-
-
-def load_codelist(filename='codelist.json') -> CodeList:
-    '''加载代码表(json格式)，未完成'''
-    codelistjson = {}
-    with open(filename,'r') as file:
-        codelistjson = json.loads(file.read())
-    codelist = CodeList()
-    codelist.elements = codelistjson['elements']
-    codelist.sections = codelistjson['sections']
-    codelist.types = codelistjson['types']
-    codelist.author = codelistjson['types']
-    codelist.description = codelistjson['description']
-    codelist.version = codelistjson['version']
-    codelist.game_version = codelistjson['game_version']
-    return codelist
+def load_codelist(filename='codelist.json') -> dict:
+    '''加载代码表(json格式),开发中'''
+    codelist = {}
+    try:
+        with open(filename,'r') as f:
+            codelist = json.loads(f.read())
+    except IOException:
+        return None
+    return CodeList(codelist)
